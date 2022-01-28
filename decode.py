cde70107647a8912ea13fff81891165c2ff00e35
sparkz11 <noahvendrig@gmail.com>
2022-01-29 02:16:51 +1100

    update tmr lol

--- decode.py
+++ decode.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 img = Image.open('encoded.png').convert('RGB')
-width, height = img.size
+width, height = img.size # not in use atm
 
 curr_pixel = 1
 decoding = True
@@ -14,15 +14,15 @@ while decoding:
         r,g,b = img.getpixel((curr_pixel, curr_pixel))
         if r == 0 or r == 1:
             curr_num += str(r)
-            curr_pixel += 10
+            curr_pixel += 2
         else:
             decoding = False
             # raise ValueError("Invalid pixel value")
             
     bin_msg.append(curr_num)
-    curr_pixel += 10
+    curr_pixel += 2
 
-print(bin_msg)
+# print(bin_msg)
 ascii_text = ""
 for i in bin_msg[:-1]: # ignore the last empty string in the array
     ascii_text += chr(int(i,2))
--- encode.py
+++ encode.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
 img = Image.open('image.png').convert('RGB')
-width, height = img.size
+width, height = img.size # not in use atm
 
-msg = "I am Noah Vendrig"
+msg = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem"
 bin_msg = [''.join(format(ord(letter), '08b')) for letter in msg] # convert each char to binary in a string
 
-print(bin_msg)
+# print(bin_msg)
 
 curr_pixel = 1
 for num in bin_msg:
@@ -14,8 +14,8 @@ for num in bin_msg:
         digit = int(digit)
         r,g,b = img.getpixel((curr_pixel, curr_pixel))
         img.putpixel((curr_pixel,curr_pixel),(digit, g, b))
-        curr_pixel += 10
-    curr_pixel += 10
+        curr_pixel += 2
+    curr_pixel += 2
 
 img.show()
 img.save("encoded.png")
